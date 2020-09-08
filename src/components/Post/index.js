87c2a349b4f1a4caf4fd59e549eb0e559d9f287b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-08 10:05:36 -0500

    Install material ui

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,19 +1,21 @@
 //import react
 import React from 'react';
 //import Image styled component
-import { Image } from './styles';
+import { Image, PostContainer, PostText } from './styles';
 
 //create post component and export it
 export const Post = () => {
 	return (
-		<>
+		<PostContainer>
 			<h3>UserName</h3>
 			{/*Header -> avatar + user name*/}
 
-			<Image src='https://colorlib.com/wp/wp-content/uploads/sites/2/react-dev-tools-logo.jpg'/>
+			<Image src='https://colorlib.com/wp/wp-content/uploads/sites/2/react-dev-tools-logo.jpg' />
 
-      <h4>UserName: Caption</h4>
+			<PostText>
+				<strong>Caption</strong>UserName:
+			</PostText>
 			{/*User name -> caption*/}
-		</>
+		</PostContainer>
 	);
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,7 +1,18 @@
 //import styled components
 import styled from 'styled-components';
 
+//create and export post container
+export const PostContainer = styled.div`
+  
+`
+
 //create and export image
 export const Image = styled.img`
-  
+  width:100%;
+  object-fit: contain;
+`
+
+//create and export post text
+export const PostText = styled.h3`
+  font-weight: normal;
 `
