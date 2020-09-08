cf196d4aa582b8c947bdfc134139c25991c2f9e9
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-08 09:21:19 -0500

    add styles to AppContainer component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,15 @@
 //import react
 import React from 'react';
+//import app component
+import { AppContainer } from './styles';
 //import header component
 import { Header } from '../Header';
 
 function App() {
 	return (
-		<>
+		<AppContainer>
 			<Header />
-		</>
+		</AppContainer>
 	);
 }
 
---
+++ src/components/App/styles.js
@@ -0,0 +1,7 @@
+//import styled component
+import styled from 'styled-components';
+
+//create app component and export it
+export const AppContainer = styled.div`
+	background-color: #fafafa;
+`;
