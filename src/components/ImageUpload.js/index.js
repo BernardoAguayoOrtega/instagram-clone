e4dbc83c9db6f42952a1e99f7983c7c6ec70c301
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-09 11:54:50 -0500

    create new component (imageUpload)

---
+++ src/components/ImageUpload.js/index.js
@@ -0,0 +1,11 @@
+//import react
+import React from 'react';
+
+//create and export functional component ImageUpload
+export const ImageUpload = () => {
+	return (
+		<>
+			<h1>I'm the image upload</h1>
+		</>
+	);
+};
