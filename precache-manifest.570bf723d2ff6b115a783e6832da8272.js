a8edf95416ce57dd6c49c060092274f654b06fa6
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-09 14:14:08 -0500

    Updates

---
+++ precache-manifest.570bf723d2ff6b115a783e6832da8272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44c41e6954f1ce9405112f7e280164a",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "baa70c72d9a853c9b633",
+    "url": "/instagram-clone/static/css/main.b23050ee.chunk.css"
+  },
+  {
+    "revision": "a6601c3475a8e955386b",
+    "url": "/instagram-clone/static/js/2.fa762719.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone/static/js/2.fa762719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa70c72d9a853c9b633",
+    "url": "/instagram-clone/static/js/main.df51dadb.chunk.js"
+  },
+  {
+    "revision": "02a87c1a75cba5191192",
+    "url": "/instagram-clone/static/js/runtime-main.c334b797.js"
+  },
+  {
+    "revision": "3ed8aa0d1e4ded479801f5352527e238",
+    "url": "/instagram-clone/static/media/Bernardogram.3ed8aa0d.png"
+  }
+]);
