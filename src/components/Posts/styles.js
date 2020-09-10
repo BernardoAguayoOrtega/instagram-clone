c71489eaf01b95a643d6eff43b25b3ed213f3cea
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-09 19:46:24 -0500

    style the list of posts

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 
 //create and import PostsContainer styled component
-export const PostsContainer = styled.div`
+export const PostsContainer = styled.div` 
   padding: 2rem;
   display:flex;
   flex-direction: column;
